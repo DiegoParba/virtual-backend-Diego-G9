88090e8bd58fc076b468df3d2fd3acd394f25553
DiegoData <diego1g@hotmail.com>
2021-08-16 21:09:29 -0500

    clase de hoy

---
+++ Dia1/02-impresion-pantalla.py
@@ -0,0 +1,20 @@
+mes= "agosto"
+print("el mes es:", mes)
+
+dia = 16
+
+print("el dia",dia, "del mes", mes)
+
+alumnos = 23
+grupos= 9
+
+print("los alumnos son {} y son del grupo {} y todos estan atentos". format(10, 5))
+
+print("los alumnos son {} y son del grupo {} y todos estan atentos". format(grupos, alumnos))
+
+print("los alumnos son {0} y son del grupo {0} y todos estan atentos". format(grupos, alumnos))
+
+#para indicar que se debe imprimir algo dentro de las llaves
+
+print(f"los alumnos son {alumnos}")
+
